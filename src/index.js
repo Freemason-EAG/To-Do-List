a87cd40a6ad88609a10457b64ca4c38f9792ffc5
Freemason-EAG <egoyan.arutyun@mail.ru>
2025-12-05 08:51:36 +0300

    feat: implement basic todo functionality

--- src/index.js
+++ src/index.js
@@ -2,36 +2,60 @@ import _ from 'lodash'
 import './style.css'
 import { htmlEscape } from 'escape-goat'
 
-const calculator = () => {
-
-    let sum = 0
-
-    const result = document.getElementById('result')
-
-    const form = document.querySelector('form')
-
-   const numberInput = document.querySelector('input[type=number]')
-   numberInput.focus()
-
-   const resetButton = document.querySelector('button')
-
-   form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    
-
-    let curNum = parseInt(numberInput.value, 10)
-    sum += curNum
-    numberInput.value = ''
-    result.textContent = sum
-    numberInput.focus()
-   })
-
-   resetButton.addEventListener('click', () => { 
-    sum = 0
-    result.textContent = sum
-    form.reset()
-    numberInput.focus()
-   }) 
+const app = () => {
+const state = {
+    tasks: [],
 }
 
-calculator()
+const form = document.getElementById('todoForm')
+
+const taskInput = form.elements.task
+const addTaskButton = document.getElementById('addTask')
+const taskList = document.getElementById('taskList')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    taskInput.focus()
+
+    const taskText = taskInput.value.trim()
+    if (taskText === '') return
+
+    const task = { text: taskText }
+    state.tasks.push(task)
+
+    const li = document.createElement('li')
+    li.textContent = task.text
+
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = "Delete"
+    deleteButton.addEventListener('click', () => {
+        const index = state.tasks.indexOf(task)
+        state.tasks.splice(index, 1)
+        li.remove()
+    })
+
+    const doneButton = document.createElement('button')
+    doneButton.textContent = "Done"
+    doneButton.addEventListener('click', () => {
+        if (doneButton.textContent === "Done") {
+        li.style.textDecoration = 'line-through'
+        doneButton.textContent = "Return task"
+        }
+        else if (doneButton.textContent === "Return task") {
+            li.style.textDecoration = 'none'
+            doneButton.textContent = "Done"
+        }
+    })
+    li.appendChild(deleteButton)
+    li.appendChild(doneButton)
+    taskList.appendChild(li)
+
+    taskInput.value = ''
+})
+
+
+}
+
+app()
+
+
